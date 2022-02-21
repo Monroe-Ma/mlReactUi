8a4d575b0774fc89968c4ac78df3ea1bf11e1184
mamenglu <2245468037@qq.com>
2022-02-21 17:08:00 +0800

    完成d.ts配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    mode: 'production',
     entry: {
         index: './lib/index.tsx'
   },
@@ -20,30 +18,9 @@ module.exports = {
             loader: 'ts-loader',
                 exclude: /node_modules/,
                 options: {
-            // configFile: './tsconfig.json', // !! WRONG
-              configFile: path.resolve('./tsconfig.json'),    // CORRECT
+              configFile: path.resolve('./tsconfig.json'), 
     },
             }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        })
-    ],
-      externals: {
-        react: {
-            commonjs: 'react',
-            commonjs2: 'react',
-            amd: 'react',
-            root: 'React',
-        },
-        'react-dom': {
-            commonjs: 'react-dom',
-            commonjs2: 'react-dom',
-            amd: 'react-dom',
-            root: 'ReactDOM',
-        },
-    }
-
 }
