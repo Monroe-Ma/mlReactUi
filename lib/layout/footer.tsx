c12d138396da63f4e8cea7457fbb41fb58ab1adb
mamenglu <2245468037@qq.com>
2022-02-23 14:53:51 +0800

    创建layout布局

---
+++ lib/layout/footer.tsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react"
+type Props = {
+  value:string
+}
+const footer: React.FC<Props> = (props) => {
+return <Fragment>
+
+  </Fragment>
+}
+export default footer
