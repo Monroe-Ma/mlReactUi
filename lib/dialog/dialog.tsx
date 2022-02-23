240891e8dffed59fd82efb8944a39576a795656f
mamenglu <2245468037@qq.com>
2022-02-23 14:14:00 +0800

    重构 dialog

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -32,7 +32,6 @@ const Dialog: React.FC<Props> = (props) => {
            return React.cloneElement(button,{ key: index })
           })}
         </footer>
-
       </div>
     </Fragment>
     : null
@@ -40,56 +39,49 @@ const Dialog: React.FC<Props> = (props) => {
     ReactDOM.createPortal(x, document.body)
     )
 }
-
-const alert = (content:string) => { 
-  const component = <Dialog visible={true}
-    onClose={() => {
-      ReactDOM.render(React.cloneElement(component,{visible:false}), div);
-      ReactDOM.unmountComponentAtNode(div)
-      div.remove()
-    }}>{content}</Dialog>
-  const div = document.createElement("div")
-  document.body.append(div)
-  ReactDOM.render(component, div);
-}  
-const confirm = (content: string, yes: () => void, no: () => void) => {
-  const onYes =()=> {
-     ReactDOM.render(React.cloneElement(component,{visible:false}), div);
-      ReactDOM.unmountComponentAtNode(div)
-    div.remove()
-    yes &&yes()
-  }
-   const onNo =()=> {
-     ReactDOM.render(React.cloneElement(component,{visible:false}), div);
-     ReactDOM.unmountComponentAtNode(div)
-    div.remove()
-    no&&no()
-  }
-  const component = <Dialog visible={true}
-    onClose={onNo}
-    buttons={[
-      <Button theme="jb-button"
-              onClick={onYes}>yes</Button>,
-      <Button theme="jb-button-primary"
-              onClick={onNo}>no</Button>,
-    ] }
-    >{content}</Dialog>
-  const div = document.createElement("div")
-    document.body.append(div)
-    ReactDOM.render(component, div);
- }
-const model = (content: ReactNode) => { 
+const model = (content: ReactNode,buttons?:Array<ReactElement>,afterClose?:()=>void  ) => { 
   const onClose = () => {
     ReactDOM.render(React.cloneElement(component, { visible: false }), div);
     ReactDOM.unmountComponentAtNode(div)
     div.remove()}
-    const component = <Dialog visible={true}
-    onClose={onClose }>{content}</Dialog>
-    const div = document.createElement("div")
-    document.body.append(div)
-    ReactDOM.render(component, div);
+    const component = <Dialog
+      visible={true}
+      onClose={() => {
+      onClose();
+      afterClose && afterClose()
+    }
+     }
+    buttons={ buttons} 
+  >{content}</Dialog>
+  
+  const div = document.createElement("div")
+  document.body.append(div)
+  ReactDOM.render(component, div);
   return onClose
 }
+
+const alert = (content: string) => { 
+  const buttons = [<Button onClick={() => {close() }} theme="jb-button" >OK</Button>]
+  const close = model(content,buttons)
+}  
+const confirm = (content: string, yes: () => void, no: () => void) => {
+  const onYes =()=> {
+    close()
+    yes &&yes()
+  }
+   const onNo =()=> {
+    close()
+    no&&no()
+   }
+   const buttons =[
+      <Button theme="jb-button"
+              onClick={onYes}>yes</Button>,
+      <Button theme="jb-button-primary" 
+              onClick={onNo}>no</Button>
+    ]
+   const close = model(content,buttons ,no)
+ }
+
 export {alert,confirm, model }
 
 export default Dialog
