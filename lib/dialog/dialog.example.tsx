910ce5bb17ff8a686863662b78732d58155af25b
mamenglu <2245468037@qq.com>
2022-02-23 13:04:20 +0800

    完成dialog功能

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,11 +1,15 @@
 import React from "react"
 import { useState } from "react"
-import Dialog from "./dialog"
+import Dialog, {alert,confirm,model } from "./dialog"
 import Button from '../Button/button';
 
 const DialogExample = () => { 
-const[visible,setVisible]=useState(false)
-
+  const [visible, setVisible] = useState(false)
+  const openModal = () => {
+    const close = model(
+      <h1>你好 <Button onClick={() => {close() }} theme="jb-button">close</Button></h1>
+  )
+   }
   return <div>
     <h1>对话框</h1>
     <Button
@@ -25,6 +29,21 @@ const[visible,setVisible]=useState(false)
             onClose={() => {setVisible(!visible) } }
             
     >some</Dialog>
+    <h4>命令式使用</h4>
+    <Button theme="jb-button-primary"
+             onClick={() => { alert("弹窗") }}
+    >alert</Button >
+
+    <Button theme="jb-button-primary"
+      onClick={() => {
+        confirm("弹窗", () => {
+        console.log("你点击了yes");
+        
+       }, () => { console.log("你点击了no"); }) }
+      } >confirm</Button >
+
+     <Button theme="jb-button-primary" onClick={openModal}>model</Button >
+
   </div>
   
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,14 +1,14 @@
-import React, { Fragment, ReactElement } from "react"
+import React, { Fragment, ReactElement, ReactNode } from "react"
 import "./dialog.scss"
 import {scopedClassMaker } from "../helper/classes"
 import Icon from '../icon/Icon';
 import  "../index.scss"
 import ReactDOM from "react-dom";
+import Button from '../Button/button';
   type Props = {
     visible: boolean
-    // onClick:()=>void
-    buttons: Array<ReactElement> 
-    onClose:()=>void
+    buttons?: Array<ReactElement> 
+    onClose?:()=>void
   }
 
 const scopedClass = scopedClassMaker("jb-dialog")
@@ -37,8 +37,59 @@ const Dialog: React.FC<Props> = (props) => {
     </Fragment>
     : null
   return (
-    ReactDOM.createPortal(
-      x, document.body)
+    ReactDOM.createPortal(x, document.body)
     )
-     }
+}
+
+const alert = (content:string) => { 
+  const component = <Dialog visible={true}
+    onClose={() => {
+      ReactDOM.render(React.cloneElement(component,{visible:false}), div);
+      ReactDOM.unmountComponentAtNode(div)
+      div.remove()
+    }}>{content}</Dialog>
+  const div = document.createElement("div")
+  document.body.append(div)
+  ReactDOM.render(component, div);
+}  
+const confirm = (content: string, yes: () => void, no: () => void) => {
+  const onYes =()=> {
+     ReactDOM.render(React.cloneElement(component,{visible:false}), div);
+      ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+    yes &&yes()
+  }
+   const onNo =()=> {
+     ReactDOM.render(React.cloneElement(component,{visible:false}), div);
+     ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+    no&&no()
+  }
+  const component = <Dialog visible={true}
+    onClose={onNo}
+    buttons={[
+      <Button theme="jb-button"
+              onClick={onYes}>yes</Button>,
+      <Button theme="jb-button-primary"
+              onClick={onNo}>no</Button>,
+    ] }
+    >{content}</Dialog>
+  const div = document.createElement("div")
+    document.body.append(div)
+    ReactDOM.render(component, div);
+ }
+const model = (content: ReactNode) => { 
+  const onClose = () => {
+    ReactDOM.render(React.cloneElement(component, { visible: false }), div);
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove()}
+    const component = <Dialog visible={true}
+    onClose={onClose }>{content}</Dialog>
+    const div = document.createElement("div")
+    document.body.append(div)
+    ReactDOM.render(component, div);
+  return onClose
+}
+export {alert,confirm, model }
+
 export default Dialog
