30152ec3e733c5ed0e3ba5723d68395e0276403a
mamenglu <2245468037@qq.com>
2022-02-26 16:04:16 +0800

    完成icon的api

--- lib/icon/icon.example.tsx
+++ lib/icon/icon.example.tsx
@@ -23,7 +23,40 @@ const IconExample = () => {
           <Demo code={( require("!!raw-loader!./icon.Color") ).default }>
         <IconColorShow />
       </Demo>
-      </div>
+    </div>
+     <div className="jb-anchor">
+       <h2>API</h2>
+    </div>
+    <table className="jb-table">
+      <thead className="jb-thead">
+        <tr>
+          <th>参数</th>
+          <th>说明</th>
+          <th>类型</th>
+          <th>默认值</th>
+        </tr>
+      </thead>
+      <tbody className="jb-tbody">
+        <tr>
+          <td>className</td>  
+          <td>设置图标的样式名</td>  
+          <td>string</td>
+          <td>-</td>  
+        </tr>
+         <tr>
+          <td>style</td>  
+          <td>设置图标的样式，例如 fontSize 和 color</td>  
+          <td>CSSProperties</td>
+          <td>-</td>  
+        </tr>
+        <tr>
+          <td>fill</td>  
+          <td>设置图标的yanse</td>  
+          <td>string</td>
+          <td>-</td>  
+        </tr>
+      </tbody>
+    </table>
   </div>
 
 }
